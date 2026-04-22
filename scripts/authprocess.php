9324630bf25ce7bfce8d32c895c0f5bfc11ffa61
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-04-22 17:10:00 +0200

    netoyer les entrer avec htmlspecialchars

--- scripts/authprocess.php
+++ scripts/authprocess.php
@@ -1,23 +1,25 @@
 <?php
-$prenom=$_POST['prenom'];
-$name=$_POST['name'];
-$email=$_POST['email'];
-$password=$_POST['password'];
+$prenom = $_POST['prenom'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 if (
     empty($name) ||
     empty($email) ||
     empty($password)
 ) {
-    echo "Tous les champs sont obligatoires ";
+    header("location:../public/register.php?error=empty");
 } else {
-    echo "Données complètes ";
+    header("location:../public/dashboard.php");
+    exit();
 }
 
 
-
-if (!filter_var($email)){
-    echo "email not correct ";
-}else{
-    echo "email coreect ";
+if (!filter_var($email)) {
+    header("location:../public/register.php?error=empty");
+    exit();
 }
-?>
+$name = htmlspecialchars($_POST['name']);
+$prenom = htmlspecialchars($_POST['$prenom']);
+$email= htmlspecialchars($_POST['$email']);
+$password = htmlspecialchars($_POST['password']);
