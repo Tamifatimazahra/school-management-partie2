37f03fa41e5ae6ec10460fa5a95a0f3bbcce2bbd
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-04-22 13:04:28 +0200

    validation de tout les champs et remplir

--- public/register.php
+++ public/register.php
@@ -25,11 +25,11 @@
               <form class="space-y-4 md:space-y-6" action="../scripts/authprocess.php" method="POST" >
                 <div>
                     <label for="name" class=""> Votre Prénom</label>
-                    <input type="text" name="prenom" id="pre" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Prénom" required="">
+                    <input type="text" name="prenom" id="pre" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Prénom" >
                 </div>
                 <div>
                     <label for="name" class="">Votre Nom</label>
-                    <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nom" required="">
+                    <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nom" >
                 </div>
                   <div>
                       <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre email</label>
--- scripts/authprocess.php
+++ scripts/authprocess.php
@@ -3,4 +3,14 @@ $prenom=$_POST['prenom'];
 $name=$_POST['name'];
 $email=$_POST['email'];
 $password=$_POST['password'];
+if (
+    empty($name) ||
+    empty($email) ||
+    empty($password)
+) {
+    echo "Tous les champs sont obligatoires ";
+} else {
+    echo "Données complètes ";
+}
+
 ?>
